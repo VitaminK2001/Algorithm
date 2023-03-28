9175db1cc907d2ec3fcc83c64cdcc31898a9c4c0
VitaminK2001 <1820011129@qq.com>
2023-03-28 11:38:06 +0800

    update

--- Search/binary.cpp
+++ Search/binary.cpp
@@ -9,7 +9,7 @@ int binarySearch(vector<int>& arr, int n, int target) {
     while(left <= right) {
         if(arr[middle] == target) {
             return middle;
-        }else if(arr[left] < target) {
+        }else if(arr[middle] < target) {
             left++;
         }else {
             right--;
@@ -19,15 +19,16 @@ int binarySearch(vector<int>& arr, int n, int target) {
 }
 
 // 寻找下界
+// 找下界 则抛弃右半部分 所以right变化的条件是 >=
 int binarySearchLower(vector<int>& arr, int target) {
     int n = arr.size();
     int left = 0, right = arr.size()-1;
     while(left <= right) { // 结束条件 left = right+1
         int mid = left+(right-left)/2;
-        if(arr[mid] < target) {
-            left = mid+1;
-        }else if(arr[mid] >= target) { // mid及以后的区间都是不需要的
-            right = mid-1;
+        if(arr[mid] < target) { // mid+1是最左边的可能等于target的位置
+            left = mid+1; 
+        }else if(arr[mid] >= target) { // mid及以后的区间都不需要
+            right = mid-1; 
         }
     }
     if(left >= n || arr[left] != target) return -1;
@@ -35,6 +36,7 @@ int binarySearchLower(vector<int>& arr, int target) {
 }
 
 // 寻找上界
+// 找上界 则抛弃左半部分 所以left变化的条件是 <=
 int binarySearchUpper(vector<int>& arr, int target) {
     int n = arr.size();
     int left = 0, right = arr.size()-1;
@@ -42,7 +44,7 @@ int binarySearchUpper(vector<int>& arr, int target) {
         int mid = left+(right-left)/2;
         if(arr[mid] <= target) { // mid及之前的区间都是不需要的
             left = mid+1;
-        }else if(arr[mid] > target) {
+        }else if(arr[mid] > target) { // mid-1是最右边的可能等于target的位置
             right = mid-1;
         }
     }
