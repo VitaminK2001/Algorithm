26dea40acdce29ac64b8ae2baf526ee00e760f95
VitaminK2001 <1820011129@qq.com>
2023-03-29 17:07:21 +0800

    update binary.cpp

--- Search/binary.cpp
+++ Search/binary.cpp
@@ -52,15 +52,45 @@ int binarySearchUpper(vector<int>& arr, int target) {
     return right;
 }
 
+// 寻找下界
+int binarySearchLowerBound(vector<int>& arr, int target) {
+    int n = arr.size();
+    int left = 0, right = n;
+    while(left < right) {
+        int mid = (left + right) >> 1;
+        if(arr[mid] < target) { // 寻找第一个大于等于的
+            left = mid+1;
+        }else { // right所在的位置永远是大于等于的
+            right = mid;
+        }
+    }
+    return left;
+}
+
+int binarySearchUpperBound(vector<int>& arr, int target) {
+    int n = arr.size();
+    int left = 0, right = n;
+    while(left < right) {
+        int mid = (left + right) >> 1;
+        if(arr[mid] <= target) { // 寻找最后一个大于等于的
+            left = mid+1;
+        }else { // left的位置永远是小于等于的
+            right = mid;
+        }
+    }
+    return left;
+}
+
+
 int main() {
     vector<int> arr = {1, 3, 4, 6, 6, 6, 6, 8, 9, 11, 11, 12};
-    int target = 6;
+    int target = 4;
 
-    int lower = binarySearchLower(arr, target);
-
-    int upper = binarySearchUpper(arr, target);
+    int lower = binarySearchLowerBound(arr, target);
 
     cout << "lower bound index is : " << lower << endl;
+
+    int upper = binarySearchUpperBound(arr, target);
 
     cout << "upper bound index is : " << upper << endl;
 
